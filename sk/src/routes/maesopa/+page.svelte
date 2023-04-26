847e90617538e7b3ef473b132aca250b64d1a635
ktunprasert <ktunprasert@outlook.com>
2023-04-26 17:29:12 +0100

    routes: removed maesopa

--- sk/src/routes/maesopa/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  import type { PBListResponse } from "$responses";
-  import type { Product } from "$t";
-  import type { PageData } from "./$types";
-  export let data: PageData & { json: PBListResponse<Product> };
-</script>
-
-<h1>Maesopa Products</h1>
-<pre>{JSON.stringify(data, null, 2)}</pre>
